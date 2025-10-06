d8489e23a859bb614db0044b0597b746c338b9dd
Susumu ISHIGAMI <susumu.ishigami@gmail.com>
2025-10-06 19:38:51 +0900

    ruff format

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,15 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information -----------------------------------------------------
-project = '東海道箱根のミニ研究論文'
-copyright = '2024'
-author = 'Author'
-release = '0.1'
+project = "東海道箱根のミニ研究論文"
+copyright = "2024"
+author = "Author"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
-    'myst_parser',
-    'sphinxcontrib.bibtex',
+    "myst_parser",
+    "sphinxcontrib.bibtex",
 ]
 
 # MyST configuration
@@ -21,23 +21,23 @@ myst_enable_extensions = [
 ]
 
 # BibTeX configuration
-bibtex_bibfiles = ['refs.bib']
+bibtex_bibfiles = ["refs.bib"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-language = 'ja'
+language = "ja"
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
 
 # -- Options for LaTeX output ------------------------------------------------
-latex_engine = 'xelatex'
+latex_engine = "xelatex"
 latex_elements = {
-    'papersize': 'a4paper',
-    'pointsize': '11pt',
-    'preamble': r'''
+    "papersize": "a4paper",
+    "pointsize": "11pt",
+    "preamble": r"""
 \usepackage{xeCJK}
 \usepackage{fontspec}
 % macOSの標準的な日本語フォントを使用
@@ -48,13 +48,12 @@ latex_elements = {
 \setsansfont{Arial}
 \setmonofont{Menlo}
 \xeCJKsetup{CJKmath=true}
-''',
-    'fncychap': '',
-    'babel': '',
-    'polyglossia': '',
+""",
+    "fncychap": "",
+    "babel": "",
+    "polyglossia": "",
 }
 
 latex_documents = [
-    ('index', 'hakone-geo-walker.tex', '東海道箱根のミニ研究論文',
-     'Author', 'manual'),
+    ("index", "hakone-geo-walker.tex", "東海道箱根のミニ研究論文", "Author", "manual"),
 ]
